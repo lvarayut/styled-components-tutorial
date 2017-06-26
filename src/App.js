d0b41f5f69f7db9e4596345676c93d8d899930ad
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2017-06-26 15:33:15 +0200

    Step-3 Style component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Title = styled.h1`
-  color: ${props => props.color ||  'goldenrod'}
+  color: ${props => props.color ||  'goldenrod'};
+  font-size: 2.5em;
 `;
 
 class App extends Component {
   render() {
     return (
-      <Title color="wheat">Gallery Application</Title>
+      <div className={this.props.className}>
+        <Title color="wheat">Gallery Application</Title>
+      </div>
     );
   }
 }
 
-export default App;
+export default styled(App)`
+  background-color: chocolate;
+  width: 100vw;
+  height: 100vh;
+  text-align: center;
+  padding-top: 30px;
+`;
+
