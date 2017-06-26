428e8faf8acc0a092a81965f8bc238c0fbdce15d
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2017-06-26 18:33:30 +0200

    Step-4 Global style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { injectGlobal } from 'styled-components';
+
 import App from './App';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+// Global style
+// eslint-disable-next-line
+injectGlobal`
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: cursive;
+  }
+`
